2b501c85e4effe67cc5e42f481d0a3bec0750754
Chris Harvey <ctharve@gmail.com>
2014-09-01 19:22:32 -0400

    completed methods in BasicAnalysis

--- SGD/src/main/java/analysis/BasicAnalysis.java
+++ SGD/src/main/java/analysis/BasicAnalysis.java
@@ -12,9 +12,29 @@ public class BasicAnalysis {
 	 * @param dataset
 	 * @return
 	 */
-	public Set<Integer> uniqTokens(DataSet dataset) {
-		// Fill in your code here
-		return null;
+	public Set<Integer> uniqTokens(DataSet dataset) {	
+	       int count = 0;
+	       Set<Integer> ids = new HashSet<Integer>();
+	       
+	       while(dataset.hasNext()) {
+	         DataInstance instance = dataset.nextInstance();
+		 int[] tokens = instance.tokens;
+		 
+		 for (int ii=0; ii<tokens.length; ii++) {
+		     ids.add(tokens[ii]);
+		 }
+
+		 count++;
+	         
+		 if (count % 100000 == 0) {
+	             System.err.println("Loaded " + count + " lines");
+	         }
+	       }
+
+	       // Important: remember to reset the dataset everytime
+	       dataset.reset();	
+	       
+	       return ids;
 	}
 	
 	/**
@@ -26,6 +46,8 @@ public class BasicAnalysis {
 	 */
 	public Map<Integer, Set<Integer>> uniqUsersPerAgeGroup(DataSet dataset) {
 		// Fill in your code here
+	        // scan entire dataset and for each observation index the Map with 
+	        // the age group and insert the userid if unique
 		return null;
 	}
 
@@ -33,8 +55,21 @@ public class BasicAnalysis {
 	 * @return the average CTR for the training set.
 	 */
 	public double averageCtr(DataSet dataset) {
-		// Fill in your code here
-		return 0.0;
+	       int clickSum = 0;	
+	       int numExamples = 0;
+	       while(dataset.hasNext()) {
+		   DataInstance instance = dataset.nextInstance();
+		   clickSum += instance.clicked;
+		   numExamples++;
+		   if (numExamples % 100000 == 0) {
+		       System.err.println("Loaded " + numExamples + " lines");
+		   }
+	       }
+
+	       double average = ((double) clickSum) / numExamples;
+	       // Important: remember to reset the dataset everytime
+	       dataset.reset();	
+	       return average;
 	}
 
 	public static void main(String args[]) throws Exception {
