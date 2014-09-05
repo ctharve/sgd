a3c135b4f553e4f72679d645fe25d17f0539167e
Chris Harvey <ctharve@gmail.com>
2014-09-05 00:16:18 -0400

    SGD implementation w/o L2 norm

--- SGD/src/main/java/analysis/LogisticRegression.java
+++ SGD/src/main/java/analysis/LogisticRegression.java
@@ -74,15 +74,34 @@ public class LogisticRegression {
 
 	/**
 	 * Helper function to compute inner product w^Tx.
-	 * 
+	 *
+         * @author charvey
+	 *
 	 * @param weights
 	 * @param instance
 	 * @return
 	 */
 	private double computeWeightFeatureProduct(Weights weights,
 			DataInstance instance) {
-		// Fill in your code here
-		return 0.0;
+	    // Fill in your code here
+	    // w0 | wAge | wGender | wDepth | wPosition | wTokens;
+	    double tokenSum = 0;
+	    int[] instTokens = instance.tokens;
+            Map<Integer, Double> allTokens = weights.wTokens;
+
+	    for (int ii=0; ii<instTokens.length; ii++) {
+		int thisInst = instTokens[ii];
+		if(allTokens.containsKey(thisInst)){
+		    tokenSum += allTokens.get(thisInst);
+		}
+	    }
+
+	    Double innerP  = 1  + weights.wAge*instance.age 
+		                + weights.wGender*instance.gender 
+		                + weights.wDepth*instance.depth 
+		                + weights.wPosition*instance.position
+		                + tokenSum;
+	    return innerP;
 	}
 
 	/**
@@ -103,28 +122,53 @@ public class LogisticRegression {
 	/**
 	 * Train the logistic regression model using the training data and the
 	 * hyperparameters. Return the weights, and record the cumulative loss.
-	 * 
+	 *
+	 * @author charvey
+	 *
 	 * @param dataset
 	 * @param lambda
 	 * @param step
 	 * @return the weights for the model.
 	 */
 	public Weights train(DataSet dataset, double lambda, double step, ArrayList<Double> AvgLoss) {
+	    int count = 0;
+	    Weights myWeights = new  Weights();
+	    double loss = 0;
+	    double avLoss; 
+
+	    while (dataset.hasNext()) {
+		count++;
+		DataInstance instance = dataset.nextInstance();
+		//performDelayedRegularization(instance.tokens, myWeights, 9, step, lambda);
+		double ip = Math.exp(computeWeightFeatureProduct(myWeights, instance));
+		double y_hat = ip / (1 + ip);
+		double y = instance.clicked;
+		loss += Math.pow((y_hat - y), 2);
 		
+		if(count % 100 == 0) {
+		    avLoss = loss / count; 
+		    AvgLoss.add(loss);
+		}
+
+		double diff = y-y_hat;
+		myWeights.w0 += step * (1*diff);;
+		myWeights.wAge += step * (instance.age*diff);
+		myWeights.wPosition += step * (instance.position*diff);
+	        myWeights.wGender += step * (instance.gender*diff);
+		myWeights.wDepth += step * (instance.depth*diff);
+	    }
+	    
+	    myWeights.toString();
+	    dataset.reset();
+	    return myWeights;
+	
 		// Fill in your code here. The structure should look like:
-		
 		// For each data point: {
-  			
     		// Your code: perform delayed regularization
-  			
-  			// Your code: predict the label, record the loss
-  		  
+      			// Your code: predict the label, record the loss	  
   			// Your code: compute w0 + <w, x>, and gradient
-  			  		
   			// Your code: update weights along the negative gradient
-		
 		// }
-  		return null;
 	}
 
 	/**
